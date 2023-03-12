bbbe7aa365fc53d2b06b546bbf46233f24de3ced
Guy Moins <moins@helha.be>
2023-03-12 09:44:52 +0100

    nouveau test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+/**
+ * The main class
+ * Lorem ipsum dolor sit amet, consectetur
+ * adipisicing elit. Qui dicta minus molestiae vel beatae natus
+ * eveniet ratione temporibus aperiam harum alias officiis
+ * @author Guy Moins
+ * @version 0.0.1
+ *
+ */
 public class Main {
+    /**
+     * The main entry method
+     * Lorem ipsum dolor sit amet, consectetur
+     * adipisicing elit. Qui dicta minus molestiae vel beatae natus
+     * eveniet ratione temporibus aperiam harum alias officiis
+     * assumenda officia quibusdam deleniti
+     * eos cupiditate dolore doloribus!
+     * @param args  the parameters passed
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world 4 !");
+        System.out.println("Hello world 7 !");
     }
 }
